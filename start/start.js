5d112a8b93cf7fea3b5b1bf9e2b95cc0af970881
Vasipalli <83403698+eshangonemad@users.noreply.github.com>
2023-09-13 16:02:04 +0800

    Add files via upload

---
+++ start/start.js
@@ -0,0 +1,65 @@
+var chemi =0
+var biol =0
+var mathext =0
+var phys =0
+var cos =0
+
+function ccl(){
+  if (chemi==0){
+    chemi=1
+    document.getElementById("chem").classList.toggle("myStyle");
+
+  }else if (chemi==1){
+    chemi=0   
+    document.getElementById("chem").classList.toggle("myStyle");
+  }
+}
+function pcl(){
+  if (phys==0){
+    phys=1
+    document.getElementById("phy").classList.toggle("myStyle");
+
+  }else if (phys==1){
+    phys=0   
+    document.getElementById("phy").classList.toggle("myStyle");
+  }
+}
+function cscl(){
+  if (cos==0){
+    cos=1
+    document.getElementById("cs").classList.toggle("myStyle");
+
+  }else if (cos==1){
+    cos=0   
+    document.getElementById("cs").classList.toggle("myStyle");
+  }
+}
+function bcl(){
+  if (biol==0){
+    biol=1
+    document.getElementById("bio").classList.toggle("myStyle");
+
+  }else if (biol==1){
+    biol=0   
+    document.getElementById("bio").classList.toggle("myStyle");
+  }
+}
+function mxcl(){
+  if (mathext==0){
+    mathext=1
+    document.getElementById("mathex").classList.toggle("myStyle");
+
+  }else if (mathext==1){
+    mathext=0   
+    document.getElementById("mathex").classList.toggle("myStyle");
+  }
+}
+
+function validate(){
+  localStorage.setItem("chemi", chemi);
+  localStorage.setItem("biol", biol);
+  localStorage.setItem("phys", phys);
+  localStorage.setItem("mathext",mathext );
+  localStorage.setItem("cos", cos);
+  window.location.assign('/index.html')
+}
