2882b0f00b623d6e168d76988973c3757102b223
Vasipalli <83403698+eshangonemad@users.noreply.github.com>
2023-10-31 10:32:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ function Giauth(){
   var vercode = prompt("School Abbreviated Name in Caps")
   if (vercode=="GIIS"){
     document.getElementById('load').src = "https://drive.google.com/embeddedfolderview?id=1pzAgePa2g4MjMEhgXcJfdODNBGLL5BSw#grid";
+  }else{
+    alert('School Not In Database')
   }
     
   }
